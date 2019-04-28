051a3e9fbafb24b6335d80ff17abda8ab4cae4d9
unknown <almaqtarif@gmail.com>
2019-04-28 20:04:36 +0300

    first commit

---
+++ src/components/ErrorPage404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorPage404 = () => {
+  return (
+    <div className="error-message">
+      <p>Error Page - 404: This Page Not Found!</p>
+    </div>
+  )
+}
+
+export default ErrorPage404
